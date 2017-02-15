8daae86a7dabc36496da310616f3a9e441490b75
danielberenberg <djberenb@uvm.edu>
2017-02-14 19:54:59 -0500

    updated database

--- warmup_build_db.py
+++ warmup_build_db.py
@@ -53,28 +53,34 @@ class BuildDB:
         num_turns = 0
         
         #diff 1
-        ts = "00:00:00 - 2/13/2017"
-        ts_elapsed = '0:00:00 - 2/13/2017'
-        user_win = 0
-        longest_draw = 1
-        avg_per_request = 1
-        difficulty = 1
-        num_turns = 0
+        ts1 = "00:00:00 - 2/13/2017"
+        ts_elapsed1 = '0:00:00 - 2/13/2017'
+        user_win1 = 0
+        longest_draw1 = 1
+        avg_per_request1 = 1
+        difficulty1 = 1
+        num_turns1 = 0
         
         #diff 2
-        #diff 0
-        ts = "00:00:00 - 2/13/2017"
-        ts_elapsed = '0:00:00 - 2/13/2017'
-        user_win = 0
-        longest_draw = 1
-        avg_per_request = 1
-        difficulty = 2
-        num_turns = 0
+        ts2 = "00:00:00 - 2/13/2017"
+        ts_elapsed2 = '0:00:00 - 2/13/2017'
+        user_win2 = 0
+        longest_draw2 = 1
+        avg_per_request2 = 1
+        difficulty2 = 2
+        num_turns2 = 0
         
         curs.execute("INSERT INTO game_stats (ts_begin,ts_elapsed, user_win,"+
                             "longest_draw, avg_per_request, difficulty,num_turns)"+
                             " VALUES (?,?,?,?,?,?,?)",(ts,ts_elapsed,user_win,longest_draw,avg_per_request,difficulty,num_turns))
         
+        curs.execute("INSERT INTO game_stats (ts_begin,ts_elapsed, user_win,"+
+                            "longest_draw, avg_per_request, difficulty,num_turns)"+
+                            " VALUES (?,?,?,?,?,?,?)",(ts1,ts_elapsed1,user_win1,longest_draw1,avg_per_request1,difficulty1,num_turns1))
+        
+        curs.execute("INSERT INTO game_stats (ts_begin,ts_elapsed, user_win,"+
+                            "longest_draw, avg_per_request, difficulty,num_turns)"+
+                            " VALUES (?,?,?,?,?,?,?)",(ts2,ts_elapsed2,user_win2,longest_draw2,avg_per_request2,difficulty2,num_turns2))
         conn.commit()
         
         #close the connection
